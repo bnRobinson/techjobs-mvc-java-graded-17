3001ab5af19116edd20aeb1e69df05b10fa50a81
Brittany Robinson <bncopple@gmail.com>
2023-11-05 20:11:41 -0600

    tried and failed Task 3

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -1,12 +1,19 @@
 package org.launchcode.techjobsmvc.controllers;
 
+import org.launchcode.techjobsmvc.models.Job;
+import org.launchcode.techjobsmvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Objects;
 
 import static org.launchcode.techjobsmvc.controllers.ListController.columnChoices;
+
 
 
 /**
@@ -16,19 +23,24 @@ import static org.launchcode.techjobsmvc.controllers.ListController.columnChoice
 @RequestMapping("search")
 public class SearchController {
 
+
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
-
-//@PostMapping
-//    public String displaySearchResults(Model model, ){
-//if (searchTerm== "" || searchTerm== "all"){
-//    jobData.findAll();
-
+    @PostMapping
+    public ArrayList<Job> displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+        // TODO #3 - Create a handler to process a search request and render the updated search view.
+        ArrayList<Job> jobs = new ArrayList<Job>();
+        if (Objects.equals(searchTerm, "") || Objects.equals(searchTerm, "all")) {
+            jobs =JobData.findAll();
+        } else {
+            jobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        return jobs;
+    }
 }
 
 
